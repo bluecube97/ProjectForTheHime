4faa2c6f4581dc0e518b56a781463fefca4a3cc5
bluecube <jph04071@gmail.com>
2024-03-26 12:11:08 +0900

    db 연결 완

--- PRJFTH/Assets/Script/UI/InitUserManager.cs
+++ PRJFTH/Assets/Script/UI/InitUserManager.cs
@@ -2,45 +2,56 @@ using System;
 using MySql.Data.MySqlClient;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 public class InitUserManager : MonoBehaviour
 {
+    public InputField InputUserNameField;
+    public Dropdown InputUserSexDropDown;
+
+    // MySQL 연결 문자열 설정
+    private string con = "Server=localhost;Database=projectforthehime;Uid=root;Pwd=1234";
+
     // EnterUser 버튼 눌리면
     public void OnClickEnterUserBtn()
     {
-        // MySQL 연결 문자열 설정
-        var con = "Server=localhost;Database=projectforthehime;Uid=root;Pwd=1234";
+        var userName = InputUserNameField.text;
+        var userSex = InputUserSexDropDown.options[InputUserSexDropDown.value].text;
 
         // MySqlConnection 객체 생성
-        var connection = new MySqlConnection(con);
-        try
+        using (var connection = new MySqlConnection(con))
         {
-            // 쿼리문
-            var sql = " INSERT INTO userdb(USERNAME, USERSEX) " +
-                      " VALUE ('?', '?') ";
-            
-            // MySqlCommand 객체 설정
-            var cmd = new MySqlCommand(sql, connection);
-            
-            // 값을 입력할 컬럼
-            cmd.Parameters.AddWithValue("@USERNAME", '?');
-            cmd.Parameters.AddWithValue("@USERSEX", '?');
+            try
+            {
+                // 쿼리문
+                var sql = " INSERT INTO userdb(USERNM, USERSEX) " +
+                          " VALUES (@userNm, @userSex) ";
 
-            // MySQL DB 연결
-            connection.Open();
-            int rowsAffected = cmd.ExecuteNonQuery();
-            connection.Close();
+                // MySqlCommand 객체 설정
+                using (var cmd = new MySqlCommand(sql, connection))
+                {
+                    // 값을 입력할 컬럼
 
+                    cmd.Parameters.AddWithValue("@userNm", userName);
+                    cmd.Parameters.AddWithValue("@userSex", userSex);
 
+                    // MySQL DB 연결
+                    connection.Open();
+                    var rowsAffected = cmd.ExecuteNonQuery();
+                    Debug.Log("Inserted " + rowsAffected + " rows into userdb.");
+                    connection.Close();
+                }
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex);
+                Debug.LogError("MySQL Connection error: " + ex.Message);
+                throw;
+            }
+
+            // 씬 불러오기
+            SceneManager.LoadScene("MainLevel_S");
         }
-        catch (MySqlException ex)
-        {
-            Console.WriteLine(ex);
-            Debug.LogError("MySQL Connection error: " + ex.Message);
-            throw;
-        }
-        
-        // 씬 불러오기
-        SceneManager.LoadScene("MainLevel_S");
     }
 }
