9fbcd774f625892f5af982b7c6f24fcacc430408
bluecube <jph04071@gmail.com>
2024-04-18 17:39:06 +0900

    add mysql

--- PRJFTH/Assets/Script/UI/initUserManager.cs
+++ PRJFTH/Assets/Script/UI/initUserManager.cs
@@ -2,10 +2,16 @@ using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using MySql.Data.MySqlClient;
 
 public class initUserManager : MonoBehaviour
 {
+    public static
     // mySql 연결
+    private void Start()
+    {
+
+    }
 
     public void OnClickEnterUserBtn()
     {
