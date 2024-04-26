a7ebd8b6181e8c31749a71ee1f8174d149eb4089
jjjch0723 <83578340+jjjch0723@users.noreply.github.com>
2024-04-26 18:08:27 +0900

    24.4.26
    24.4.26

--- PRJFTH/Assets/Script/UI/HospitalManager.cs
+++ PRJFTH/Assets/Script/UI/HospitalManager.cs
@@ -7,4 +7,106 @@ public class HospitalManager : MonoBehaviour
     {
         SceneManager.LoadScene("OutingScene");
     }
-}
+    
+
+
+
+
+    public void OnClickCure()
+   {
+        ActivateCureMenu(); // Cure 메뉴를 엶
+    }
+
+     public void OnClickCureYes()
+    {
+        Debug.Log("치료되었습니다");  
+        DeactivateCureMenu();
+    }
+
+     public void OnClickCureNo()
+    {
+    DeactivateCureMenu(); //cure 메뉴를 끔
+    }
+
+
+    public GameObject CureMenuBackGround; // 설정 패널 오브젝트
+
+    private void StartCure()
+    {
+        // 게임 시작 시 설정 패널을 비활성화
+        CureMenuBackGround.SetActive(false);
+    }
+
+    private void ActivateCureMenu()    //작동시 활성화
+    {
+        CureMenuBackGround.SetActive(true);
+    }
+
+    private void DeactivateCureMenu()   //작동시 비활성화
+    {
+        CureMenuBackGround.SetActive(false);
+    }
+
+
+
+
+
+      public void OpenBuy()
+   {
+        ActivateBuyMenu(); // 구매 메뉴를 엶
+    }
+
+    public void CloseBuy()
+   {
+        DeactivateBuyMenu   (); // 구매 메뉴를 엶
+    }
+
+    public GameObject HospitalBuyBackGround; // 설정 패널 오브젝트
+
+    private void StartBuy()
+    {
+        // 게임 시작 시 설정 패널을 비활성화
+        HospitalBuyBackGround.SetActive(false);
+    }
+
+    private void ActivateBuyMenu()    //작동시 활성화
+    {
+        HospitalBuyBackGround.SetActive(true);
+    }
+
+    private void DeactivateBuyMenu()   //작동시 비활성화
+    {
+        HospitalBuyBackGround.SetActive(false);
+    }
+
+
+
+
+      public void OpenSell()
+    {
+        ActivateSellMenu(); // 판매 메뉴를 엶
+    }
+
+    public void CloseSell()
+    {
+        DeactivateSellMenu(); // 판매 메뉴를 엶
+    }
+
+    public GameObject HospitalSellBackGround; // 설정 패널 오브젝트
+
+    private void Start()
+    {
+        // 게임 시작 시 설정 패널을 비활성화
+        HospitalSellBackGround.SetActive(false);
+    }
+
+    private void ActivateSellMenu()    //작동시 활성화
+    {
+        HospitalSellBackGround.SetActive(true);
+    }
+
+    private void DeactivateSellMenu()   //작동시 비활성화
+    {
+        HospitalSellBackGround.SetActive(false);
+    }
+} 
--- PRJFTH/Assets/Script/UI/VarietyStoreManager.cs
+++ PRJFTH/Assets/Script/UI/VarietyStoreManager.cs
@@ -7,4 +7,66 @@ public class VarietyStoreManager : MonoBehaviour
     {
         SceneManager.LoadScene("OutingScene");
     }
-}
+
+
+
+
+ public void OpenBuy()
+   {
+        ActivateBuyMenu(); // 구매 메뉴를 엶
+    }
+
+    public void CloseBuy()
+   {
+        DeactivateBuyMenu   (); // 구매 메뉴를 엶
+    }
+
+    public GameObject VarietyStoreBuyBackGround; // 설정 패널 오브젝트
+
+    private void StartBuy()
+    {
+        // 게임 시작 시 설정 패널을 비활성화
+        VarietyStoreBuyBackGround.SetActive(false);
+    }
+
+    private void ActivateBuyMenu()    //작동시 활성화
+    {
+        VarietyStoreBuyBackGround.SetActive(true);
+    }
+
+    private void DeactivateBuyMenu()   //작동시 비활성화
+    {
+        VarietyStoreBuyBackGround.SetActive(false);
+    }
+
+
+
+
+      public void OpenSell()
+    {
+        ActivateSellMenu(); // 판매 메뉴를 엶
+    }
+
+    public void CloseSell()
+    {
+        DeactivateSellMenu(); // 판매 메뉴를 엶
+    }
+
+    public GameObject VarietyStoreSellBackGround; // 설정 패널 오브젝트
+
+    private void Start()
+    {
+        // 게임 시작 시 설정 패널을 비활성화
+        VarietyStoreSellBackGround.SetActive(false);
+    }
+
+    private void ActivateSellMenu()    //작동시 활성화
+    {
+        VarietyStoreSellBackGround.SetActive(true);
+    }
+
+    private void DeactivateSellMenu()   //작동시 비활성화
+    {
+        VarietyStoreSellBackGround.SetActive(false);
+    }
+} 
