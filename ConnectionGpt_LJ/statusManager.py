1a0e8f57c5c97fdc44a7f4c664d6301899fd669e
E__mergency <limju0091@gmail.com>
2024-05-02 17:01:47 +0900

    5.2 수정내용
    getter. setter이용해서 스텟 넣고 빼기 실패..(˘･_･˘)
    
    딸 마지막 대화에 출력된 모든 스탯 업데이트에 올리기 성공

--- ConnectionGpt_LJ/statusManager.py
+++ ConnectionGpt_LJ/statusManager.py
@@ -22,139 +22,139 @@ class Daughter:
         self._P = None
 
     @property
-    def get_Name(self):
+    def name(self):
         return self._name
 
-    @get_Name.setter
-    def set_Name(self, value):
+    @name.setter
+    def name(self, value):
         self._name = value
 
     @property
-    def get_Age(self):
+    def age(self):
         return self._age
 
-    @get_Age.setter
-    def set_Age(self, value):
+    @age.setter
+    def age(self, value):
         self._age = value
 
     @property
-    def get_Sex(self):
+    def sex(self):
         return self._sex
 
-    @get_Sex.setter
-    def set_Sex(self, value):
+    @sex.setter
+    def sex(self, value):
         self._sex = value
 
     @property
-    def get_Mbti(self):
+    def mbti(self):
         return self._mbti
 
-    @get_Mbti.setter
-    def set_Mbti(self, value):
+    @mbti.setter
+    def mbti(self, value):
         self._mbti = value
 
     @property
-    def get_Hp(self):
+    def hp(self):
         return self._hp
 
-    @get_Hp.setter
-    def set_Hp(self, value):
+    @hp.setter
+    def hp(self, value):
         self._hp = value
 
     @property
-    def get_Mp(self):
+    def mp(self):
         return self._mp
 
-    @get_Mp.setter
-    def set_Mp(self, value):
+    @mp.setter
+    def mp(self, value):
         self._mp = value
 
     @property
-    def get_Mood(self):
+    def mood(self):
         return self._mood
 
-    @get_Mood.setter
-    def set_Mood(self, value):
+    @mood.setter
+    def mood(self, value):
         self._mood = value
 
     @property
-    def get_Stress(self):
+    def stress(self):
         return self._stress
 
-    @get_Stress.setter
-    def set_Stress(self, value):
+    @stress.setter
+    def stress(self, value):
         self._stress = value
 
     @property
-    def get_Fatigue(self):
+    def fatigue(self):
         return self._fatigue
 
-    @get_Fatigue.setter
-    def set_Fatigue(self, value):
+    @fatigue.setter
+    def fatigue(self, value):
         self._fatigue = value
 
     @property
-    def get_E(self):
+    def E(self):
         return self._E
 
-    @get_E.setter
-    def set_E(self, value):
+    @E.setter
+    def E(self, value):
         self._E = value
 
     @property
-    def get_I(self):
+    def I(self):
         return self._I
 
-    @get_I.setter
-    def set_I(self, value):
+    @I.setter
+    def I(self, value):
         self._I = value
 
     @property
-    def get_S(self):
+    def S(self):
         return self._S
 
-    @get_S.setter
-    def set_S(self, value):
+    @S.setter
+    def S(self, value):
         self._S = value
 
     @property
-    def get_N(self):
+    def N(self):
         return self._N
 
-    @get_N.setter
-    def set_N(self, value):
+    @N.setter
+    def N(self, value):
         self._N = value
 
     @property
-    def get_T(self):
+    def T(self):
         return self._T
 
-    @get_T.setter
-    def set_T(self, value):
+    @T.setter
+    def T(self, value):
         self._T = value
 
     @property
-    def get_F(self):
+    def F(self):
         return self._F
 
-    @get_F.setter
-    def set_F(self, value):
+    @F.setter
+    def F(self, value):
         self._F = value
 
     @property
-    def get_J(self):
+    def J(self):
         return self._J
 
-    @get_J.setter
-    def set_J(self, value):
+    @J.setter
+    def J(self, value):
         self._J = value
 
     @property
-    def get_P(self):
+    def P(self):
         return self._P
 
-    @get_P.setter
-    def set_P(self, value):
+    @P.setter
+    def P(self, value):
         self._P = value
 
 
@@ -180,39 +180,42 @@ class Daughter:
 
 #d = Daughter() 
 
-def load_daughter_status(daughter, communication_path):
+def load_daughter_status():
     daughter = Daughter()
+    ##파일 경로 설정
+    communication_path = os.path.join("conversationData", "daughter_status.json")
     with open(communication_path, 'r', encoding='utf-8') as f:
         items = json.load(f)
 
     status_keys = items["daughter"]
 
     # 예외처리 key값이 없을경우
-    daughter.set_Name = status_keys["name"]
-    daughter.set_Age = status_keys["age"]
-    daughter.set_Sex = status_keys["sex"]
-    daughter.set_Mbti = status_keys["mbti"]
-    daughter.set_Hp = status_keys["hp"]
-    daughter.set_Mp = status_keys["mp"]
-    daughter.set_Mood = status_keys["mood"]
-    daughter.set_Stress = status_keys["stress"]
-    daughter.set_Fatigue = status_keys["fatigue"]
-    daughter.set_E = status_keys["E"]
-    daughter.set_I = status_keys["I"]
-    daughter.set_S = status_keys["S"]
-    daughter.set_N = status_keys["N"]
-    daughter.set_T = status_keys["T"]
-    daughter.set_F = status_keys["F"]
-    daughter.set_J = status_keys["J"]
-    daughter.set_P = status_keys["P"]
+    daughter.name = status_keys["name"]
+    daughter.age = status_keys["age"]
+    daughter.sex = status_keys["sex"]
+    daughter.mbti = status_keys["mbti"]
+    daughter.hp = status_keys["hp"]
+    daughter.mp = status_keys["mp"]
+    daughter.mood = status_keys["mood"]
+    daughter.stress = status_keys["stress"]
+    daughter.fatigue = status_keys["fatigue"]
+    daughter.E = status_keys["E"]
+    daughter.I = status_keys["I"]
+    daughter.S = status_keys["S"]
+    daughter.N = status_keys["N"]
+    daughter.T = status_keys["T"]
+    daughter.F = status_keys["F"]
+    daughter.J = status_keys["J"]
+    daughter.P = status_keys["P"]
+
+    #print("딸랑구 나이 : ", daughter.age)
+    #print("딸랑구 이름 : ", daughter.name)  # 이제 업데이트된 age가 출력됩니다
 
     return daughter
 
-d = Daughter()
+#d = Daughter()
 
-##파일 경로 설정
-communication_path = os.path.join("conversationData", "daughter_status.json")
-d = load_daughter_status(d, communication_path)
 
-print("딸랑구 나이 : ", d.get_Age)
-print("딸랑구 이름 : ", d.get_Name)  # 이제 업데이트된 age가 출력됩니다
+#load_daughter_status(Daughter())
+
+
