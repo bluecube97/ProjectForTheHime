ebfb9ee81fb34f8a21e08152506d0a216e9a3061
aelraim <aelraim@gmail.com>
2024-05-07 16:37:55 +0900

    PGH_MakeClothingMenu

---
+++ projFTH/Assets/Script/UI/Outing/ClothingStore/ClothingDao.cs
@@ -0,0 +1,140 @@
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Ubiety.Dns.Core;
+using UnityEngine;
+
+public class ClothingDao : MonoBehaviour
+{
+    private string con = "Server=localhost;Database=testdb;Uid=root;Pwd=1234;Charset=utf8mb4";
+
+    public List<ClothingVO> GetClothingList()
+    {
+        List<ClothingVO> clothingList = new();
+
+
+        string sql = "SELECT CLONO, CLONM, SILKCNT, LINECNT, BUYFLAG" +
+                     "  FROM clothing c ";
+
+        using (MySqlConnection connection = new MySqlConnection(con))
+        {
+            connection.Open();
+            using (MySqlCommand cmd = connection.CreateCommand())
+            {
+                cmd.CommandText = sql;
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    cmd.Parameters.Clear();
+
+                    while (reader.Read())
+                    {
+                        ClothingVO cv = new ClothingVO();
+                        cv.cloNo = reader.GetInt32("CLONO");
+                        cv.cloNm = reader.GetString("CLONM");
+                        cv.slikCnt = reader.GetInt32("SILKCNT");
+                        cv.linCnt = reader.GetInt32("LINECNT");
+                        cv.buyFlag = reader.GetString("BUYFLAG");
+                        clothingList.Add(cv);
+                    }
+                }
+            }
+        }
+
+        return clothingList;
+    }
+    public List<ClothingVO> GetClothingBuyList()
+    {
+        List<ClothingVO> CltBuyList = new();
+
+
+        string sql = "  SELECT SEQ, i.ID AS ItemNo, i.Name AS ItemName, i.Description AS ItemDescription, cts.SELLPRICE AS SellPrice " +
+                        " FROM clothingsell AS cts " +
+                       " INNER JOIN Item AS i ON cts.ItemID = i.ID ";
+
+        using (MySqlConnection connection = new MySqlConnection(con))
+        {
+            connection.Open();
+            using (MySqlCommand cmd = connection.CreateCommand())
+            {
+                cmd.CommandText = sql;
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    cmd.Parameters.Clear();
+
+                    while (reader.Read())
+                    {
+                        ClothingVO cv = new ClothingVO();
+                        cv.seq = reader.GetInt32("SEQ");
+                        cv.clsNo = reader.GetInt32("ItemNo");
+                        cv.clsNm = reader.GetString("ItemName");
+                        cv.clsDes = reader.GetString("ItemDescription");
+                        cv.clspri = reader.GetInt32("SellPrice");
+                        CltBuyList.Add(cv);
+                    }
+                }
+            }
+        }
+
+        return CltBuyList;
+    }
+
+    public void Buyclothing(int index)
+    {
+        string sql = "  UPDATE clothing " +
+                         " SET BUYFLAG = 'Y' " +
+                       " WHERE CLONO =  @clothingNo";
+        using (MySqlConnection connection = new MySqlConnection(con))
+        {
+            connection.Open();
+            using (MySqlCommand cmd = connection.CreateCommand())
+            {
+                cmd.Parameters.Clear();
+                cmd.CommandText = sql;
+                cmd.Parameters.AddWithValue("@clothingNo", index + 1);
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+    public int GetUserInfoFromDB()
+    {
+        int Usercash = 0;
+        var sql = "  SELECT gu.USERCASH " +
+                 "   FROM game_userinfo gu ";
+        using (MySqlConnection connection = new MySqlConnection(con))
+        {
+            connection.Open();
+            using (MySqlCommand cmd = connection.CreateCommand())
+            {
+                cmd.CommandText = sql;
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        Usercash = reader.GetInt32(0);
+                        Debug.Log(Usercash);
+                    }
+                }
+            }
+        }
+        return Usercash;
+    }
+    public void UpdateUserCash(int payment)
+    {
+
+        using (MySqlConnection connection = new MySqlConnection(con))
+        {
+            connection.Open();
+            using (MySqlCommand cmd = connection.CreateCommand())
+            {
+                var sql = " update game_userinfo " +
+                             " set USERCASH = (@payment)" +
+                           " where SEQ = 1 ";
+                // DB에 유저 정보 저장
+                cmd.CommandText = sql;
+                cmd.Parameters.AddWithValue("@payment", payment);
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ projFTH/Assets/Script/UI/Outing/ClothingStore/ClothingStoreManager.cs
@@ -0,0 +1,170 @@
+using Script.UI.Outing;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.WSA;
+
+public class ClothingStoreManager : MonoBehaviour
+{
+    public GameObject ClothingPrefab; // 옷 제작 이미지 프리팹 참조
+    public GameObject Clothing; // 옷 제작 이미지 참조
+    public Transform ClothingLayout; // 제작 리스트들이 들어갈 레이아웃 참조
+    private List<GameObject> ClothingInstances = new List<GameObject>();
+
+    public GameObject ClotBuyPrefab; // 옷가게 판매 이미지 프리팹 참조
+    public GameObject ClotBuy; // 옷가게 판매 이미지 참조
+    public Transform ClotBuyLayout; // 판매 리스트들이 들어갈 레이아웃 참조
+    private List<GameObject> ClotBuyInstances = new List<GameObject>();
+
+    private ClothingDao clothingDao;
+    private InventoryDao inventoryDao;
+    private ClothingUIManager clothingUIManager;
+
+    private List<ClothingVO> clothingList = new List<ClothingVO>();
+    private List<ClothingVO> cltBuyList = new List<ClothingVO>();
+    private List<InventoryVO> invenList = new List<InventoryVO>();
+
+    private int slikCnt = 0;
+    private int lineCnt = 0;
+    private int index = 0;
+    private int Buyprice = 0;
+
+    void Start()
+    {
+        clothingDao = GetComponent<ClothingDao>();
+        inventoryDao = GetComponent<InventoryDao>();
+        clothingUIManager = FindObjectOfType<ClothingUIManager>();
+
+        clothingList = clothingDao.GetClothingList();
+        cltBuyList = clothingDao.GetClothingBuyList();
+        invenList = inventoryDao.GetInvenList();
+
+        SetClothingList(clothingList);
+        SetCltBuyList(cltBuyList);
+    }
+
+    private void SetCltBuyList(List<ClothingVO> cltBuyList)
+    {
+        foreach (GameObject cloteBuyInstance in ClotBuyInstances)
+        {
+            Destroy(cloteBuyInstance);
+        }
+        ClotBuyInstances.Clear();
+        foreach (var cls in cltBuyList)
+        {
+            GameObject clotBuyInstance = Instantiate(ClotBuyPrefab, ClotBuyLayout);
+            clotBuyInstance.name = "Clothing" + cls.seq;
+            ClotBuyInstances.Add(clotBuyInstance);
+            Text textComponent = clotBuyInstance.GetComponentInChildren<Text>();
+            if (textComponent != null)
+            {
+                textComponent.text = cls.clsNm + "\r\n" +
+                                     cls.clsDes + "\r\n" +
+                                     "가격 : " + cls.clspri;
+            }
+        }
+        ClotBuy.SetActive(false);
+    }
+    private void SetClothingList(List<ClothingVO> clothingList)
+    {
+        foreach (GameObject clothingInstances in ClothingInstances)
+        {
+            Destroy(clothingInstances);
+        }
+        ClothingInstances.Clear();
+
+        foreach (var clo in clothingList)
+        {
+            if (clo.buyFlag.Equals("N"))
+            {
+
+                GameObject clothingInstance = Instantiate(ClothingPrefab, ClothingLayout);
+                clothingInstance.name = "Clothing" + clo.cloNo;
+                ClothingInstances.Add(clothingInstance);
+
+                Text textComponent = clothingInstance.GetComponentInChildren<Text>();
+                if (textComponent != null)
+                {
+                    textComponent.text = clo.cloNm
+                                        + "\r\n" + "옷감 요구량" + clo.slikCnt
+                                        + "\r\n" + "실 요구량" + clo.linCnt;
+                }
+            }
+        }
+        Clothing.SetActive(false);
+    }
+    public void GetClothingValue()
+    {
+        GameObject clickedButton = EventSystem.current.currentSelectedGameObject;
+        GameObject parentObject = clickedButton.transform.parent.gameObject;
+        string parentObjectName = parentObject.name;
+        string indexString = parentObjectName.Replace("Clothing", "");
+        index = int.Parse(indexString) - 1;
+
+        ClothingVO clv = new();
+
+        clv = clothingList[index];
+        slikCnt = clv.slikCnt;
+        lineCnt = clv.linCnt;
+        Debug.Log(slikCnt);
+    }
+    public void GetClotBuyValue()
+    {
+        GameObject clickedButton = EventSystem.current.currentSelectedGameObject;
+        GameObject parentObject = clickedButton.transform.parent.gameObject;
+        string parentObjectName = parentObject.name;
+        string indexString = parentObjectName.Replace("Clothing", "");
+        index = int.Parse(indexString) - 1;
+
+        ClothingVO clv = new();
+        clv = cltBuyList[index];
+        Buyprice = clv.clspri;
+        Debug.Log(Buyprice);
+    }
+        public void BuyClothing()
+    {
+        // 여러 번의 데이터베이스 액세스를 단일 액세스로 변경
+        var slik = invenList.Find(p => p.ItemNm.Equals("옷감"));
+        var line = invenList.Find(p => p.ItemNm.Equals("실"));
+
+        int balSlik = slik.ItemCnt - slikCnt;
+        int balLine = line.ItemCnt - lineCnt;
+
+        // 쿼리 결과를 한 번만 사용하도록 변경
+        if (balSlik > 0 && balLine > 0)
+        {
+            inventoryDao.BuyClothing(balSlik, balLine);
+            clothingDao.Buyclothing(index);
+            clothingUIManager.OnClickBuyComple();
+        }
+        else
+        {
+            clothingUIManager.OnClickBuyFail();
+        }
+
+       
+    }
+    public void BuyThing()
+    {
+        int userCash = clothingDao.GetUserInfoFromDB();
+        int NowCash = userCash - Buyprice;
+        Debug.Log("DB 유저 현금 " + userCash);
+        Debug.Log("계산 후 금액 " + NowCash);
+        if (NowCash > 0)
+        {
+            clothingDao.UpdateUserCash(NowCash);
+            clothingUIManager.OnClickBuyComple();
+        }
+        else
+        {
+            Debug.Log("Not enough cash!");
+            clothingUIManager.OnClickBuyFail();
+
+        }
+    }
+}
