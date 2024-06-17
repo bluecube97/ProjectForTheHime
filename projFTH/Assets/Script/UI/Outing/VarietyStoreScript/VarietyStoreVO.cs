6bc5073a6975e093ecc32b232624b2a7bdf5b01b
aelraim <aelraim@gmail.com>
2024-06-17 12:04:39 +0900

    주석달기 final

--- projFTH/Assets/Script/UI/Outing/VarietyStoreScript/VarietyStoreVO.cs
+++ projFTH/Assets/Script/UI/Outing/VarietyStoreScript/VarietyStoreVO.cs
@@ -1,26 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
-public class ItemListVO { 
-    public string ITEMNO { get; set; }
-    public string TYPEID { get; set; }
-    public string ITEMNAME { get; set; }
+namespace Script.UI.Outing.VarietyStoreScript
+{
+    public class ItemListVO { 
+        public string ITEMNO { get; set; }
+        public string TYPEID { get; set; }
+        public string ITEMNAME { get; set; }
     
-    public string ITEMDESC { get; set; }
+        public string ITEMDESC { get; set; }
 
-    public string ITEMPR { get; set; }
+        public string ITEMPR { get; set; }
 
-    public ItemListVO()
-    {
-    }
-    public ItemListVO(string itemno, string typeid, string itemnm, string itemsdesc, string itempr)
-    {
-        ITEMNO = itemno;
-        TYPEID = typeid;
-        ITEMNAME = itemnm;
-        ITEMDESC = itemsdesc;
-        ITEMPR = itempr;
-        
     }
 }
