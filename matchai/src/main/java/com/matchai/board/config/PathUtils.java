35626939fb40876d3913c58a991f4d1039af2bf8
jjjch0723 <83578340+jjjch0723@users.noreply.github.com>
2024-06-28 16:04:59 +0900

    [Upadte] API 동작 완료

---
+++ matchai/src/main/java/com/matchai/board/config/PathUtils.java
@@ -0,0 +1,65 @@
+package com.matchai.board.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "file")
+public class PathUtils {
+
+    @Value("${file.path}")
+    private String path;
+    @Value("${file.mlb-schedule-py-path}")
+    private String mlbSchedulePyPath;
+    @Value("${file.kbo-schedule-py-path}")
+    private String kboSchedulePyPath;
+    @Value("${file.mlb-result-py-path}")
+    private String mlbResultPyPath;
+    @Value("${file.kbo-result-py-path}")
+    private String kboResultPyPath;
+
+
+    // 게터 세터 생성. 혹은 lombok사용
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getMlbSchedulePyPath() {
+        return mlbSchedulePyPath;
+    }
+
+    public void setMlbSchedulePyPath(String mlbSchedulePyPath) {
+        this.mlbSchedulePyPath = mlbSchedulePyPath;
+    }
+
+    public String getKboSchedulePyPath() {
+        return kboSchedulePyPath;
+    }
+
+    public void setKboSchedulePyPath(String kboSchedulePyPath) {
+        this.kboSchedulePyPath = kboSchedulePyPath;
+    }
+
+    public String getMlbResultPyPath() {
+        return mlbResultPyPath;
+    }
+
+    public void setMlbResultPyPath(String mlbResultPyPath) {
+        this.mlbResultPyPath = mlbResultPyPath;
+    }
+
+    public String getKboResultPyPath() {
+        return kboResultPyPath;
+    }
+
+    public void setKboResultPyPath(String kboResultPyPath) {
+        this.kboResultPyPath = kboResultPyPath;
+    }
+
+}
