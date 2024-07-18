df2b08bb13f22bd7249c3dbc10e692b8e049761f
jjjch0723 <83578340+jjjch0723@users.noreply.github.com>
2024-07-18 12:59:27 +0900

    asd
    asd

--- matchai/src/main/java/com/matchai/board/mapper/BoardMapper.java
+++ matchai/src/main/java/com/matchai/board/mapper/BoardMapper.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface BoardMapper {
