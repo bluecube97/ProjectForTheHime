c7690f5b180ac0d15e60cfa9f8b1de056db3328b
aelraim <aelraim@gmail.com>
2024-07-23 12:11:30 +0900

    xml rstore

--- matchai/src/main/webapp/resource/js/mainboard.js
+++ matchai/src/main/webapp/resource/js/mainboard.js
@@ -57,13 +57,12 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 
-
-    function startUnity() {
-        window.location.href = '/board/unity';
-    }
-
-    function loginPlz() {
-        alert("로그인 후 사용가능 합니다.")
-    }
-
 });
+
+function startUnity() {
+    window.location.href = '/board/unity';
+}
+
+function loginPlz() {
+    alert("로그인 후 사용가능 합니다.")
+}
