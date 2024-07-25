cc5a368441cec112ca699e48b671f937fae391f5
bluecube <jph04071@gmail.com>
2024-07-25 18:52:20 +0900

    Battle Scene

---
+++ projFTH/Assets/Script/3D/UI/BattleUI.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Script._3D.UI
+{
+    public class BattleUI : MonoBehaviour
+    {
+        public GameObject battleCanvas;
+        public GameObject behaviorInstance;
+        public GameObject behaviorPrefab;
+        public GameObject behaviorLayout;
+        public void StartBattle()
+        {
+            Debug.Log("Battle Start");
+            battleCanvas.SetActive(true);
+            SetBehavior("Attack");
+        }
+
+        private void SetBehavior(string action)
+        {
+            behaviorPrefab.SetActive(true);
+            for (int i = 0; i < 8; i++)
+            {
+                behaviorInstance = Instantiate(behaviorPrefab, behaviorLayout.transform);
+                Text behaviorText = behaviorInstance.GetComponentInChildren<Text>();
+                behaviorText.text = action + " " + i;
+            }
+            behaviorPrefab.SetActive(false);
+        }
+
+        private void RemoveBehavior()
+        {
+            foreach (Transform child in behaviorLayout.transform)
+            {
+                if (!child.gameObject.activeSelf) continue;
+                Destroy(child.gameObject);
+            }
+        }
+
+        public void OnClickAttackBtn()
+        {
+            RemoveBehavior();
+            SetBehavior("Attack");
+        }
+
+        public void OnClickSkillBtn()
+        {
+            RemoveBehavior();
+            SetBehavior("Skill");
+        }
+
+        public void OnClickItemBtn()
+        {
+            RemoveBehavior();
+            SetBehavior("Item");
+        }
+
+        public void OnClickRunBtn()
+        {
+            RemoveBehavior();
+            SetBehavior("Run");
+        }
+    }
+}
